d749c71707f97225688d9c66934e7228fed8e142
paomedia <paomedia@users.noreply.github.com>
2014-05-14 18:05:59 +0200

    substr fix

--- bin/concat.json.php
+++ bin/concat.json.php
@@ -6,7 +6,7 @@ function start()
   $data = array();
   foreach(glob("../svg/*.svg") as $file)
     {
-      $data[] = array('name' => substr(basename($file), 0, -3),
+      $data[] = array('name' => substr(basename($file), 0, -4),
 		      'blob' => base64_encode(file_get_contents($file)));
     }
   file_put_contents('../concat.js', 'printIcons(' . json_encode($data) . ');' . "\n");
