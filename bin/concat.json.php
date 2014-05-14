fe5a962541eb307a12edbb85a39c9d8438ee7100
paomedia <paomedia@users.noreply.github.com>
2014-05-14 13:57:31 +0200

    jsonp data

---
+++ bin/concat.json.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+start();
+function start()
+{
+  $data = array();
+  foreach(glob("../svg/*.svg") as $file)
+    {
+      $data[] = array('name' => substr(basename($file), 0, -3),
+		      'blob' => base64_encode(file_get_contents($file)));
+    }
+  
+  file_put_contents('../concat.json', json_encode($data));
+}
