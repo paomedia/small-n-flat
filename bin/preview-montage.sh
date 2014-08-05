ded8db7dcd9e0128c141b5b10072ca783d1fb52b
paomedia <paomedia@users.noreply.github.com>
2014-08-06 01:44:48 +0200

    fixing svgo + montage rendering bug

--- bin/preview-montage.sh
+++ bin/preview-montage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-montage -geometry +16+16 ../svg/*.svg ../preview-24.png
-montage -background none -density 144 -tile 8 -geometry +16+16 ../svg/*.svg ../preview-48.png
+montage -geometry +16+16 ../src/*.svg ../preview-24.png
+montage -background none -density 144 -tile 8 -geometry +16+16 ../src/*.svg ../preview-48.png
 #montage -background none -density 288 -tile 6 -geometry +64+64 ../svg/*.svg ../preview-96.png
