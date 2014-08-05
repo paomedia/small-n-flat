6840f883d8bda5074bd2c1c486bbd00e464f2d00
paomedia <paomedia@users.noreply.github.com>
2014-08-06 01:38:15 +0200

    size optimization

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+git rm -r ../svg/
+git rm -r ../png/
+
+mkdir ../svg ../png ../png/24 ../png/48 ../png/72 ../png/96
+
 ./source-to-svg.sh
 ./svg-to-png.sh
 ./concat.json.php
 ./preview-montage.sh
+
+git add --all
--- bin/source-to-svg.sh
+++ bin/source-to-svg.sh
@@ -4,11 +4,14 @@ FILES=../src/*.svg
 
 for f in $FILES
 do
-    scour -i $f -o ../svg/$(basename "$f") \
-	--create-groups \
-	--enable-id-stripping \
-	--enable-comment-stripping \
-	--shorten-ids \
-	--disable-embed-rasters \
-	--remove-metadata
+#    scour -i $f -o ../svg/$(basename "$f") \
+#	--create-groups \
+#	--enable-id-stripping \
+#	--enable-comment-stripping \
+#	--shorten-ids \
+#	--disable-embed-rasters \
+#	--remove-metadata
+
+svgo -i $f -o ../svg/$(basename "$f")
+
 done
